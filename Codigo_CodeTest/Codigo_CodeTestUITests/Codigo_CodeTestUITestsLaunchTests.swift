426360ae501f0d328f12b487cd18d631e852174e
Min <zinminphyo@neversitup.com>
2023-05-28 09:12:17 +0630

    Project setup.

---
+++ Codigo_CodeTest/Codigo_CodeTestUITests/Codigo_CodeTestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Codigo_CodeTestUITestsLaunchTests.swift
+//  Codigo_CodeTestUITests
+//
+//  Created by Zin Min Phyo on 28/05/2023.
+//
+
+import XCTest
+
+final class Codigo_CodeTestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
